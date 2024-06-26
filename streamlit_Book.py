713f51273e1b515a7fc790050414236d25608836
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-06-26 22:41:08 +0530

    Update streamlit_Book.py

--- streamlit_Book.py
+++ streamlit_Book.py
@@ -9,4 +9,62 @@ book_name = pickle.load(open('Book_Recommender/book_name.pkl', 'rb'))
 final_rating = pickle.load(open('Book_Recommender/final_rating.pkl', 'rb'))
 book_pivot = pickle.load(open('Book_Recommender/book_pivot.pkl', 'rb'))
 
+def fetch_poster(suggestion):
+  book_name = []
+  ids_index = []
+  poster_url = []
+  for bookd_id in suggestion:
+    book_name.append(book_pivot.index[book_id])
+  for name in book_name[0]:
+    ids = np.where(final_rating['Book-Title'] == name)[0][0]
+    ids_index.append(ids)
+  for idx in ids_index:
+    url = final_rating.iloc[idx]['Image-URL-S']
+    poster_url.append(url)
+
+
+
+  
+
+def book_recommend(book_name):
+  book_list = []
+  
+  book_id = np.where(book_pivot.index == book_name)[0][0]
+  distance , suggestion = model.kneighbors(book_pivot.iloc[book_id,:].values.reshape(1,-1),n_neighbors=6)
+  poster_url = fetch_poster(suggestion)
+  for i in range(len(suggestion)):
+    books = books_pivot.index[suggestion[i]]
+    for j in books:
+      book_list.append(j)
+      
+    
+    
+
+
+
+
 selected_books = st.selectbox("Type or Select a Book ", book_name )
+
+if st.button('Show Recommendation'):
+  recommendation_books, poster_url = book_recommend(selected_books)
+  col1 , col2 , col3 , col4 , col5 = st.column(5)
+  with col1:
+    st.text(recommendation_books[1])
+    st.image(poster_url[1])
+  with col2:
+    st.text(recommendation_books[2])
+    st.image(poster_url[2])
+  with col3:
+    st.text(recommendation_books[3])
+    st.image(poster_url[3])
+  with col4:
+    st.text(recommendation_books[4])
+    st.image(poster_url[4])
+  with col5:
+    st.text(recommendation_books[5])
+    st.image(poster_url[5])
+
+
+
+
+
