716a74206454a406166b14b87527c2edad48651e
Pratik-Abhang <147050620+Pratik-Abhang@users.noreply.github.com>
2024-06-26 22:08:29 +0530

    Update streamlit_Book.py

--- streamlit_Book.py
+++ streamlit_Book.py
@@ -4,9 +4,9 @@ import numpy as np
 import pandas as pd 
 
 st.header("Book Recommendation App")
-model = pickle.load(open('C:\Users\PRATIK\Book_Recommender/model.pkl', 'rb'))
-book_name = pickle.load(open('C:\Users\PRATIK\Book_Recommender/book_name.pkl', 'rb'))
-final_rating = pickle.load(open('C:\Users\PRATIK\Book_Recommender/final_rating.pkl', 'rb'))
-book_pivot = pickle.load(open('C:\Users\PRATIK\Book_Recommender/book_pivot.pkl', 'rb'))
+model = pickle.load(open(r'C:\Users\PRATIK\Book_Recommender/model.pkl', 'rb'))
+book_name = pickle.load(open(r'C:\Users\PRATIK\Book_Recommender/book_name.pkl', 'rb'))
+final_rating = pickle.load(open(r'C:\Users\PRATIK\Book_Recommender/final_rating.pkl', 'rb'))
+book_pivot = pickle.load(open(r'C:\Users\PRATIK\Book_Recommender/book_pivot.pkl', 'rb'))
 
 selected_books = st.selectbox("Type or Select a Book ", book_name )
